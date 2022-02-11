df0b37abb03b24b710e65523eb4a328f7d15a757
Darion Richards <darionrichards@outlook.com>
2022-02-11 09:41:02 +0000

    Created thoughtData

---
+++ src/seeds/data/thoughts.js
@@ -0,0 +1,116 @@
+const thoughtData = [
+	{
+		thoughtText: "JavaScript is fun!!",
+		username: "Bazza",
+		reactions: [
+			{
+				reactionBody: "I think so too.",
+				username: "Sammy",
+			},
+			{
+				reactionBody: "It really is, especially working with arrays.",
+				username: "Mikey",
+			},
+		],
+	},
+	{
+		thoughtText: "Off to play golf!",
+		username: "Mikey",
+		reactions: [
+			{
+				reactionBody: "Have fun!",
+				username: "Gaz",
+			},
+			{
+				reactionBody: "Hope you break 100!",
+				username: "Bazza",
+			},
+		],
+	},
+	{
+		thoughtText: "I need of a holiday to America!",
+		username: "Haz",
+		reactions: [
+			{
+				reactionBody: "America is so beautiful!",
+				username: "Carly",
+			},
+			{
+				reactionBody: "I visited California in December is was awsome!",
+				username: "Mikey",
+			},
+		],
+	},
+	{
+		thoughtText: "Can't wait to learn react",
+		username: "Carly",
+		reactions: [
+			{
+				reactionBody: "Its an amazing framework!",
+				username: "Haz",
+			},
+			{
+				reactionBody: "Me too!",
+				username: "Gaz",
+			},
+		],
+	},
+	{
+		thoughtText: "Off for a jog!",
+		username: "Gaz",
+		reactions: [
+			{
+				reactionBody: "Message me, we could jog together!",
+				username: "Haz",
+			},
+			{
+				reactionBody: "How far do you jog?",
+				username: "Sammy",
+			},
+		],
+	},
+	{
+		thoughtText: "Can't wait to try Hickorys Smokehouse later",
+		username: "Sammy",
+		reactions: [
+			{
+				reactionBody: "Make sure to get the ribs.",
+				username: "Carly",
+			},
+			{
+				reactionBody: "Oooo the ribs are mouth watering!",
+				username: "Bazza",
+			},
+		],
+	},
+	{
+		thoughtText: "NoSQL is alot of fun!",
+		username: "Bazza",
+		reactions: [
+			{
+				reactionBody: "Sure makes things easier!",
+				username: "Mikey",
+			},
+			{
+				reactionBody: "I love it!",
+				username: "Sammy",
+			},
+		],
+	},
+	{
+		thoughtText: "Off to play golf!",
+		username: "Sammy",
+		reactions: [
+			{
+				reactionBody: "Have fun!",
+				username: "Gaz",
+			},
+			{
+				reactionBody: "Hope you break 100!",
+				username: "Bazza",
+			},
+		],
+	},
+];
+
+module.exports = thoughtData;
