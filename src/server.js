6f9c676edb35ff691f38dcd3af3f8a875b082df5
Darion Richards <darionrichards@outlook.com>
2022-02-11 09:42:24 +0000

    Created connection to DB when running server

--- src/server.js
+++ src/server.js
@@ -1,18 +1,30 @@
 const express = require("express");
 const routes = require("./routes");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 4000;
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(routes);
 
-const init = () => {
-    app.listen(PORT, () => {
-        console.log(`🚀🚀 Server running on http://localhost:${PORT} 🚀🚀`);
-    });
+const init = async () => {
+	try {
+		await mongoose.connect("mongodb://localhost:27017/socialMediaApiDb", {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+
+		console.log("[INFO]: Connection to DB successful");
+
+		app.listen(PORT, () => {
+			console.log(`🚀🚀 Server running on http://localhost:${PORT} 🚀🚀`);
+		});
+	} catch (error) {
+		console.log(`[ERROR]: Connection to DB unsuccessful | ${error.message}`);
+	}
 };
 
 init();
