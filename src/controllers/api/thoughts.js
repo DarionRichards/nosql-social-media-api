7f1e124524df7bdb59ca62b5eb5bb580123eef68
Darion Richards <darionrichards@outlook.com>
2022-02-11 11:58:03 +0000

    Created controller to getThoughts
    - Returns all thoughts

--- src/controllers/api/thoughts.js
+++ src/controllers/api/thoughts.js
@@ -1,23 +1,37 @@
-const getThoughts = (req, res) => {
-    res.send("getThoughts");
+const {Thought} = require("../../models");
+
+const getThoughts = async (req, res) => {
+	try {
+		const thoughts = await Thought.find({});
+		return res.status(200).json({
+			success: true,
+			data: thoughts,
+		});
+	} catch (error) {
+		return res.status(500).json({
+			success: false,
+			message: "Failed to get thoughts",
+			error: error.message,
+		});
+	}
 };
 const getThoughtById = (req, res) => {
-    res.send("getThoughtById");
+	res.send("getThoughtById");
 };
 const createThought = (req, res) => {
-    res.send("createThought");
+	res.send("createThought");
 };
 const updateThoughtById = (req, res) => {
-    res.send("updateThoughtById");
+	res.send("updateThoughtById");
 };
 const deleteThoughtById = (req, res) => {
-    res.send("deleteThoughtById");
+	res.send("deleteThoughtById");
 };
 
 module.exports = {
-    getThoughts,
-    getThoughtById,
-    createThought,
-    updateThoughtById,
-    deleteThoughtById,
+	getThoughts,
+	getThoughtById,
+	createThought,
+	updateThoughtById,
+	deleteThoughtById,
 };
