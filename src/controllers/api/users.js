4f3551eb05838e24440048a766e0bfc200600579
Darion Richards <darionrichards@outlook.com>
2022-02-11 09:45:00 +0000

    Created controller to getUsers
    Returns all users

--- src/controllers/api/users.js
+++ src/controllers/api/users.js
@@ -1,23 +1,38 @@
-const getUsers = (req, res) => {
-    res.send("getUsers");
+const {User} = require("../../models");
+
+const getUsers = async (req, res) => {
+	try {
+		const users = await User.find({});
+		return res.status(200).json({
+			success: true,
+			data: users,
+		});
+	} catch (error) {
+		return res.json({
+			success: false,
+			message: "Failed to get users",
+			error: error.message,
+		});
+	}
 };
+
 const getUserById = (req, res) => {
-    res.send("getUserById");
+	res.send("getUserById");
 };
 const createUser = (req, res) => {
-    res.send("createUser");
+	res.send("createUser");
 };
 const updateUserById = (req, res) => {
-    res.send("updateUserById");
+	res.send("updateUserById");
 };
 const deleteUserById = (req, res) => {
-    res.send("deleteUserById");
+	res.send("deleteUserById");
 };
 
 module.exports = {
-    getUsers,
-    getUserById,
-    createUser,
-    updateUserById,
-    deleteUserById,
+	getUsers,
+	getUserById,
+	createUser,
+	updateUserById,
+	deleteUserById,
 };
