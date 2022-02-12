a47b5967f0c17667c395f516b2b3d42f57449726
Darion Richards <darionrichards@outlook.com>
2022-02-12 23:27:23 +0000

    Created controller to addFriend

--- src/controllers/api/friends.js
+++ src/controllers/api/friends.js
@@ -1,11 +1,36 @@
-const addFriend = (req, res) => {
-    res.send("addFriend");
+const {User} = require("../../models");
+
+const addFriend = async (req, res) => {
+	try {
+		const {userId, friendId} = req.params;
+
+		const user = await User.findByIdAndUpdate(
+			userId,
+			{
+				$push: {friends: friendId},
+			},
+			{
+				new: true,
+			}
+		);
+
+		return res.status(200).json({
+			success: true,
+			data: user,
+		});
+	} catch (error) {
+		return res.status(500).json({
+			success: false,
+			message: "Failed to add friend",
+			error: error.message,
+		});
+	}
 };
 const deleteFriendById = (req, res) => {
-    res.send("deleteFriendById");
+	res.send("deleteFriendById");
 };
 
 module.exports = {
-    addFriend,
-    deleteFriendById,
+	addFriend,
+	deleteFriendById,
 };
