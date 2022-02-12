2a86e7be6694a2a79926e1255594e4470e60e49e
Darion Richards <darionrichards@outlook.com>
2022-02-12 15:46:20 +0000

    Create controller to addReaction

--- src/controllers/api/reactions.js
+++ src/controllers/api/reactions.js
@@ -1,11 +1,35 @@
-const addReaction = (req, res) => {
-    res.send("addReaction");
+const {Thought} = require("../../models");
+
+const addReaction = async (req, res) => {
+	try {
+		const {thoughtId} = req.params;
+
+		const {reactions} = await Thought.findByIdAndUpdate(
+			thoughtId.trim(),
+			{
+				$push: {reactions: {...req.body}},
+			},
+			{new: true}
+		);
+
+		return res.status(200).json({
+			success: true,
+			data: reactions,
+		});
+	} catch (error) {
+		return res.status(500).json({
+			success: false,
+			message: "Could not add reaction",
+			error: error.message,
+		});
+	}
 };
-const deleteReactionById = (req, res) => {
-    res.send("deleteReactionById");
+
+const deleteReactionById = async (req, res) => {
+	res.send("deleteReactionById");
 };
 
 module.exports = {
-    addReaction,
-    deleteReactionById,
+	addReaction,
+	deleteReactionById,
 };
